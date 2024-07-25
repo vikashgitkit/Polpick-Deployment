d9a36ca3adf2fae4c031704a17af1070d771bc23
Webskitters <vikashofficial9@gmail.com>
2024-07-25 16:38:27 +0530

    Creatting pool with ethers

--- test/Polpick.js
+++ test/Polpick.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { ethers } = require("hardhat");
-const { hexlify } = ethers;
+const { hexlify, parseUnits } = ethers;
 const { toUtf8Bytes } = require("@ethersproject/strings");
 
 describe("PolPick Contract", function () {
@@ -41,17 +41,34 @@ describe("PolPick Contract", function () {
     });
 
 
-    it("Should create a pool", async function () {
+    // it("Should create a pool", async function () {
+    //   const { polPick, owner } = await loadFixture(deployPolPickFixture);
+    //   const poolId = hexlify(toUtf8Bytes("pool1"));
+    //   console.log("🚀 ~ poolId:", poolId)
+    //   await polPick.connect(owner).createPool(poolId, 100, 1000, 10);
+    //   const pool = await polPick.pools(poolId);
+      
+    //   expect(pool.created).to.equal(true);
+    //   expect(pool.minBetAmount).to.equal(100);
+    //   expect(pool.maxBetAmount).to.equal(1000);
+    //   expect(pool.poolBetsLimit).to.equal(10);
+    // });
+
+    it("Should create a pool with minimum and maximum bet amounts in ETH", async function () {
       const { polPick, owner } = await loadFixture(deployPolPickFixture);
       const poolId = hexlify(toUtf8Bytes("pool1"));
       console.log("🚀 ~ poolId:", poolId)
-      await polPick.connect(owner).createPool(poolId, 100, 1000, 10);
+      const minBetAmount = parseUnits("0.1"); // 0.1 ETH
+      const maxBetAmount = parseUnits("1.0"); // 1 ETH
+      const poolBetsLimit = 10;
+      
+      await polPick.connect(owner).createPool(poolId, minBetAmount, maxBetAmount, poolBetsLimit);
       const pool = await polPick.pools(poolId);
       
       expect(pool.created).to.equal(true);
-      expect(pool.minBetAmount).to.equal(100);
-      expect(pool.maxBetAmount).to.equal(1000);
-      expect(pool.poolBetsLimit).to.equal(10);
+      expect(pool.minBetAmount).to.equal(minBetAmount);
+      expect(pool.maxBetAmount).to.equal(maxBetAmount);
+      expect(pool.poolBetsLimit).to.equal(poolBetsLimit);
     });
 
     it("Should return true for an open pool", async function () {
@@ -66,6 +83,28 @@ describe("PolPick Contract", function () {
       const { polPick, addr1 } = await loadFixture(deployPolPickFixture);
       const poolId = hexlify(toUtf8Bytes("pool1"));
       await expect(polPick.connect(addr1).createPool(poolId, 100, 1000, 10)).to.be.revertedWith("Only game controller can do this");
+    });
+
+
+    describe("makeTrade Function", function () {
+      it("Should make a trade successfully", async function () {
+        const { polPick, owner, addr1 } = await loadFixture(deployPolPickFixture);
+        const poolId = hexlify(toUtf8Bytes("pool9"));
+        await polPick.connect(owner).createPool(poolId, 100, 1000, 10);
+        await polPick.connect(owner).startGame();
+
+        await polPick.connect(addr1).makeTrade({
+          poolId: poolId,
+          avatarUrl: "https://example.com/avatar1.png",
+          countryCode: "US",
+          upOrDown: true,
+          whiteLabelId: "WL123",
+          gameId: "GAME123"
+        }, { value: parseEther("0.2") });
+
+        const pool = await polPick.pools(poolId);
+        expect(pool.upBetGroup.total).to.equal(parseEther("0.2"));
+      });
     });
 
 
